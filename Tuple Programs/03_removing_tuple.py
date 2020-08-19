ef4c9b02c8eb57d514e5d645f9df4fa29d39b9dd
vaibhavvishal223007 <vaibhavvishal223007@gmail.com>
2020-08-19 16:19:07 +0530

    fifth day third commit

--- Tuple Programs/03_removing_tuple.py
+++ Tuple Programs/03_removing_tuple.py
@@ -21,3 +21,18 @@ def removing_tuple(input_list, length):
 result = removing_tuple([(4, 5), (4,), (8, 6, 7), (1,), (3, 4, 6, 7), (9, 8)], 2)
 print(f"Method-1 result => {result}")
 
+# While solving this problem i found that i cannot change the range() in for loop and due to which
+# i was getting "Index out of range" error. So after that i used while and it worked
+# Use while whenever you are uncertain how many times should loop work or
+# when you have to change the number of iteration while running the loop
+
+# Method-2: List comprehention
+
+
+def removing_tuple2(input_list, length):
+    new_list = [ele for ele in input_list if len(ele) != length]
+    return new_list
+
+
+result2 = removing_tuple2([(4, 5), (4,), (8, 6, 7), (1,), (3, 4, 6, 7), (9, 9)], 2)
+print(f"Method-2 result => {result2}")
